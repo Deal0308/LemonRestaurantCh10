ae52b73304b576584bace396b2f08bfe56a1138e
Michael Deal <Deal0308@yahoo.com>
2026-06-02 19:51:30 -0500

    Initial Commit

---
+++ LemonRestaurantCh10/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LemonRestaurantCh10
+//
+//  Created by Michael Deal on 6/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LemonRestaurantCh10/LemonRestaurantCh10App.swift
@@ -0,0 +1,17 @@
+//
+//  LemonRestaurantCh10App.swift
+//  LemonRestaurantCh10
+//
+//  Created by Michael Deal on 6/2/26.
+//
+
+import SwiftUI
+
+@main
+struct LemonRestaurantCh10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
