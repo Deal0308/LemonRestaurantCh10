e59015c5e2ac70bd9b25c33e39c5b336a63c1071
Michael Deal <Deal0308@yahoo.com>
2026-06-04 22:20:06 -0500

    reservation

---
+++ LemonRestaurantCh10/ReservationForm.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationForm.swift
+//  LemonRestaurantCh10
+//
+//  Created by Michael Deal on 6/4/26.
+//
+
+import SwiftUI
+
+struct ReservationForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReservationForm()
+}
