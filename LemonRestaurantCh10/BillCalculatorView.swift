de8c71b464e936e9dbc84806145af8c35ca4ecb3
Michael Deal <Deal0308@yahoo.com>
2026-06-09 21:23:01 -0500

    Final report

---
+++ LemonRestaurantCh10/BillCalculatorView.swift
@@ -0,0 +1,18 @@
+//
+//  BillCalculatorView.swift
+//  LemonRestaurantCh10
+//
+//  Created by Michael Deal on 6/9/26.
+//
+
+import SwiftUI
+
+struct BillCalculatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BillCalculatorView()
+}
