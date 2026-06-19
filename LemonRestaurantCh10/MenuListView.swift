5a9803196e815fda2f234e433333f321eb0c8c53
Michael Deal <Deal0308@yahoo.com>
2026-06-18 21:16:57 -0500

    Final Report

---
+++ LemonRestaurantCh10/MenuListView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuListView.swift
+//  LemonRestaurantCh10
+//
+//  Created by Michael Deal on 6/18/26.
+//
+
+import SwiftUI
+
+struct MenuListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuListView()
+}
