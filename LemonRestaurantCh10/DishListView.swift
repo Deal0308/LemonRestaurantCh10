d5816a01b3da76e3cd940da202429d89bf7b0b7f
Michael Deal <Deal0308@yahoo.com>
2026-06-23 21:44:59 -0500

    assignment2

---
+++ LemonRestaurantCh10/DishListView.swift
@@ -0,0 +1,18 @@
+//
+//  DishListView.swift
+//  LemonRestaurantCh10
+//
+//  Created by Michael Deal on 6/23/26.
+//
+
+import SwiftUI
+
+struct DishListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DishListView()
+}
