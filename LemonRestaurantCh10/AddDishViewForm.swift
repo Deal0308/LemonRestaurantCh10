66277d5033c5cd2f015aad59ec6e462ed0556f5c
Michael Deal <Deal0308@yahoo.com>
2026-06-27 13:40:47 -0500

    finalreport3

---
+++ LemonRestaurantCh10/AddDishViewForm.swift
@@ -0,0 +1,18 @@
+//
+//  AddDishViewForm.swift
+//  LemonRestaurantCh10
+//
+//  Created by Michael Deal on 6/27/26.
+//
+
+import SwiftUI
+
+struct AddDishViewForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddDishViewForm()
+}
